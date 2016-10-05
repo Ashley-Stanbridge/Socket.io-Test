ab8cd36bc4d0a20626fb8a3d6bfa72762811fcf5
Ashley Stanbridge <ashleystanbridge@icloud.com>
2016-10-05 17:32:37 +1100

    message rendering to dom

--- index.js
+++ index.js
@@ -7,17 +7,19 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+
+io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('disconnect', function(){
     console.log('user disconnected')
   })
 })
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg)
-  })
-})
 
 http.listen(5000, function() {
   console.log('listning on *:5000');
